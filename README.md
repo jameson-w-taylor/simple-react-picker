8baf34b7be6b301e9c0f00f7f82419c57e9dffcc
Jameson Taylor <jameson@ionic.io>
2022-03-24 12:20:16 -0400

    Add picker example; Add README;

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple IonPicker Example
+This app was created to demonstrate how to create and present an [IonPicker](https://ionicframework.com/docs/api/picker) using the `useIonPicker` hook in React.
+
+## Developer Setup
+Run the following commands to serve the app:
+```bash
+npm install
+ionic serve
+```
+
+## Usage
+Once the app is served, click the "Open Picker" button to see the picker open.
+Select a value, and click "Confirm" to see the `console.log` output with the result.
+
+<div>
+  <img src="./documentation/screenshots/Picker.png" style="height:auto;width:auto;max-height:400px;">
+</div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, useIonPicker } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [presentPicker] = useIonPicker();
+
+  const openPicker = async () => {
+    return presentPicker({
+      columns: [{
+        name: 'test',
+        options: [
+          { text: 'Value 1', value: 1 },
+          { text: 'Value 2', value: 2 }
+        ]
+      }],
+      buttons: [
+        { text: 'Cancel', role: 'cancel' },
+        { text: 'Confirm', handler: handleConfirm }
+      ]
+    })
+  };
+
+  const handleConfirm = (pickerValue: any) => {
+    console.log('Picker confirm result: ', pickerValue);
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,7 +37,9 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={openPicker}>
+          Open Picker
+        </IonButton>
       </IonContent>
     </IonPage>
   );
